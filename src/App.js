3591e611d5e8b74722998eb91c76d12658ee1739
Icaro Oliveira <icarooliv2@gmail.com>
2019-04-10 14:50:34 -0300

    moves browserouter to app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
 import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
+import Dashboard from "./components/dashboard/Dashboard";
+import Create from "./components/create/Create";
+import Details from "./components/details/Details";
+import Update from "./components/update/Update";
 
 class App extends Component {
   render() {
-    return <Navbar />;
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route
+                exact
+                path="/pokemon/details/:id"
+                component={withRouter(Details)}
+              />
+              <Route exact path="/pokemon/edit/:id" component={Update} />
+              <Route exact path="/pokemon/new" component={Create} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
-import Dashboard from "./components/dashboard/Dashboard";
-import Create from "./components/create/Create";
-import Details from "./components/details/Details";
-import Update from "./components/update/Update";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <div className="App">
-      <App />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route
-            exact
-            path="/pokemon/details/:id"
-            component={withRouter(Details)}
-          />
-          <Route exact path="/pokemon/edit/:id" component={Update} />
-          <Route exact path="/pokemon/new" component={Create} />
-        </Switch>
-      </div>
-    </div>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
