563b62fe5c776c233ff669c7c117a0c7a0f41975
Icaro Oliveira <icarooliv2@gmail.com>
2019-04-15 10:45:09 -0300

    fixes url at poketypes actions

--- src/store/actions/pokeTypesActions.js
+++ src/store/actions/pokeTypesActions.js
@@ -1,5 +1,7 @@
+import BASE_URL_V1 from "../../api/consts";
+
 export const fetchPokeTypes = () => dispatch => {
-  fetch("http://127.0.0.1:3000/api/v1/types")
+  fetch(`${BASE_URL_V1}/types`)
     .then(res => res.json())
     .then(types =>
       dispatch({
